44fc990b26b77a5e49b8cd327eea5cdfded07304
brunno <34986733+brunnuh@users.noreply.github.com>
2019-10-18 23:50:55 -0300

    Add files via upload
    18/10/19-23:50

--- cmdDaVida.c
+++ cmdDaVida.c
@@ -10,16 +10,26 @@
 #define COLOR_RESET "\x1b[0m"
 
 #define True 0
-#define Falso 1
+#define False 1
 
 void command(char *parametro){
 
 }
 
 
+void pAtual(char pastaAtual[]){
 
+    printf(COLOR_1);
+    printf ( "\nbrunnuh@brunnuh-linux:~%s$ " , getcwd(pastaAtual, 100));
+    printf(COLOR_RESET);
+}
 
+void mais(char *parametro){
+  char more[] = "more ";
+  strcat(more,parametro);
+  system(more);
 
+}
 
 
 int main(){
@@ -31,10 +41,14 @@ int main(){
   int status;
   char *cmd = "ls";
   char *usuario;
+  int flag = False;
   usuario = getenv("USER");
   system("clear");// limpando a tela
   int pipe_b[2];
-  if(pipe(pipe_b) < 0){
+  char buffer[20];
+  int sl = pipe(pipe_b);
+
+  if(sl < 0){
     printf("Erro na criacao do pipe\n" );
     return -1;
   }
@@ -50,46 +64,64 @@ int main(){
         fgets(entrada,20,stdin);
 
         parametro[0] = strtok(entrada, " "); // recebe sempre espaco em branco ou a primeira palavra
-        parametro[1] = strtok(NULL,"\n");// recebe sempre a segunda palavra ou NULL
-        if(strcmp(parametro[1],"|")){
-          subparametro[0] = strtok(parametro[1],"| ");
-          parametro[1] = strtok(NULL,"/n");
-          printf(" sb %s\npa %s\n",subparametro[0], parametro[1] );
+        parametro[1] = strtok(NULL,"\n");// \n recebe sempre a segunda palavra ou NULL
+
+        if(parametro[1] != NULL && strcmp(parametro[1],"|") == True){
+            flag = True;
+            subparametro[0] = strtok(parametro[1],"| ");
+            parametro[1] = strtok(NULL,"/n");
+            //printf(" sb %s\npa %s\n",subparametro[0], parametro[1] );
+        }else{
+          subparametro[0] = NULL;
         }
 
                 if(strcmp(parametro[0],"cd") == True){// erro se digitar somente
+
                     chdir(parametro[1]); // implementar condicao de erro para pastas que nao existem
-                    printf(COLOR_1);
-                    printf ( "brunnuh@brunnuh-linux:~%s$ " , getcwd(pastaAtual, 100));
-                    printf(COLOR_RESET);
+                    pAtual(pastaAtual);
                 }
                 else if(strcmp(parametro[0],"pwd\n") == True){
                     printf ( "%s " , getcwd(pastaAtual, 100));
-                    printf(COLOR_1);
-                    printf ( "\nbrunnuh@brunnuh-linux:~%s$ " , getcwd(pastaAtual, 100));
-                    printf(COLOR_RESET);
+                    pAtual(pastaAtual);
                 }
-                else if((strcmp(parametro[0], "ls\n") == True)){
-                    system("ls");
-                    printf(COLOR_1);
-                    printf ( "brunnuh@brunnuh-linux:~%s$ " , getcwd(pastaAtual, 100));
-                    printf(COLOR_RESET);
+                else if((strcmp(parametro[0], "ls") == True) || (strcmp(parametro[0], "ls\n") == True)){
+
+                    filho = fork();
+
+                    if(filho == 0){
+                        //printf("filho" );
+                        system("ls");
+
+                        close(pipe_b[0]) ;
+                        //printf("subparametro:%s\n", subparametro[0]);
+                        write(pipe_b[1],parametro[1],strlen(parametro[1])+1);
+
+
+                    }else{
+
+                            close(pipe_b[1]) ;
+                            read(pipe_b[0], buffer, sizeof(buffer));
+                            //printf("%s",buffer);
+                            if(strcmp(buffer,"more")){
+                              //char more[] = "more ";
+                              strcat(more,buffer);
+                              system(more);
+                              pAtual(pastaAtual);
+                            }
+
+                        }
+
+
                 }
                 else if((strcmp(parametro[0],"more") == True)){
-                    strcat(more,parametro[1]);
-                    system(more);
-                    printf("Comando Invalido");
-                    printf(COLOR_1);
-                    printf ( "\nbrunnuh@brunnuh-linux:~%s$ " , getcwd(pastaAtual, 100));
-                    printf(COLOR_RESET);
+                    mais(parametro[1]);
+                    pAtual(pastaAtual);
                 }else if((strcmp(parametro[0],"grep"))){
 
                 }
                 else{
                     printf("Comando Invalido");
-                    printf(COLOR_1);
-                    printf ( "\nbrunnuh@brunnuh-linux:~%s$ " , getcwd(pastaAtual, 100));
-                    printf(COLOR_RESET);
+                    pAtual(pastaAtual);
                 }
             }
 
